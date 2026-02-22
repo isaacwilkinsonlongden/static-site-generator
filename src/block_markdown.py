b5d5361fcacdf65457b06892608df7e46a107643
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-02-22 19:33:20 +0000

    added block_to_block_type function to block_markdown.py and unit tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown):
     blocks_unstripped = markdown.split("\n\n")
     blocks = []
@@ -6,5 +8,63 @@ def markdown_to_blocks(markdown):
         if block != "":
             blocks.append(block)
     return blocks
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+def block_to_block_type(markdown_block):
+    if markdown_block.startswith((
+        "###### ",
+        "##### ",
+        "#### ",
+        "### ",
+        "## ",
+        "# "
+    )):
+        return BlockType.HEADING
+    if markdown_block.startswith("```\n") and markdown_block.endswith("```"):
+        return BlockType.CODE
+    lines = markdown_block.split("\n")
+    ol_num = 1
+    if lines[0].startswith(">"):
+        current = "quote"
+    elif lines[0].startswith("- "):
+        current = "ul"
+    elif lines[0].startswith(f"{ol_num}. "):
+        current = "ol"
+        ol_num += 1
+    else:
+        return BlockType.PARAGRAPH
+    if current == "quote":
+        for i in range(1, len(lines)):
+            if lines[i].startswith(">"):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if current == "ul":
+        for i in range(1, len(lines)):
+            if lines[i].startswith("- "):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if current == "ol":
+        for i in range(1, len(lines)):
+            if lines[i].startswith(f"{ol_num}. "):
+                ol_num += 1
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.OLIST
+
+        
+
+
     
     
