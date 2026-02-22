138d4fad0a857cc70f904b04a1a09be9537d6cb2
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-02-22 15:08:58 +0000

    added split_nodes_image and split_node_link to inline_markdown.py and unit tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -26,4 +26,51 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            extracted_images = extract_markdown_images(node.text)
+            if len(extracted_images) == 0:
+                new_nodes.append(node)
+                continue
+            text = node.text
+            for i in range(len(extracted_images)):
+                sections = text.split(f"![{extracted_images[i][0]}]({extracted_images[i][1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("invalid markdown image syntax")
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                new_nodes.append(TextNode(extracted_images[i][0], TextType.IMAGE, extracted_images[i][1]))
+                text = sections[1]
+            if text != "":
+                new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            extracted_links = extract_markdown_links(node.text)
+            if len(extracted_links) == 0:
+                new_nodes.append(node)
+                continue
+            text = node.text
+            for i in range(len(extracted_links)):
+                sections = text.split(f"[{extracted_links[i][0]}]({extracted_links[i][1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("invalid markdown link syntax")
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                new_nodes.append(TextNode(extracted_links[i][0], TextType.LINK, extracted_links[i][1]))
+                text = sections[1]
+            if text != "":
+                new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
+
+
 
