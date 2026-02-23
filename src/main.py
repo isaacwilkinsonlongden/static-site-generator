3862dde302255ae7cbbfda32b9af933b06392c61
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-02-23 15:22:45 +0000

    created static_to_public function in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    static_to_public("static", "public")
+
+def static_to_public(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    copy_contents(source, destination)
+
+def copy_contents(source, destination):
+    file_List = os.listdir(source)
+    for file in file_List:
+        source_path = os.path.join(source, file)
+        destination_path = os.path.join(destination, file)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path) 
+            print(f"copied {source_path} to {destination_path}")
+        else:
+            os.mkdir(destination_path)
+            copy_contents(source_path, destination_path)
+
+
 
 if __name__ == "__main__":
     main()
